b838add78d7bc70cefdf8b4f4f56d3af02cf30ab
Elizaveta Dotsenko <ambriona@yandex.ru>
2024-12-08 15:15:22 +0300

    Добавила функцию precipitation_probability и weather_analysis

--- app.py
+++ app.py
@@ -43,12 +43,51 @@ def weather_by_location(location_key): # Получаем данные о пог
             "temperature_celsius": weather["Temperature"]["Metric"]["Value"], #правильный вывод
             "humidity": weather.get("RelativeHumidity"),
             "wind_speed_kmh": weather.get("Wind", {}).get("Speed", {}).get("Metric", {}).get("Value"),
-            "precipitation_probability": weather.get("PrecipitationProbability", "Not available")  # Вероятность осадков
         }
         return result, None
     except requests.RequestException as e:
         return None, str(e)
 
+def precipitation_probability(location_key): #получаем вероятности осадков из другого url
+    try:
+        url = f"{BASE_URL}forecasts/v1/hourly/1hour/{location_key}"
+        params = {
+            "apikey": API_KEY,
+            "metric": "true"  # Используем метрическую систему
+        }
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        if not data:
+            return None, "Precipitation data not found"
+
+        precipitation_probability = data[0].get("PrecipitationProbability", 0)
+        return precipitation_probability, None
+    except requests.RequestException as e:
+        return None, str(e)
+
+def weather_analysis(weather_data): # Анализируем погодные данные
+    temperature = weather_data.get("temperature_celsius")
+    wind_speed = weather_data.get("wind_speed_kmh")
+    humidity = weather_data.get("humidity")
+    weather_text = weather_data.get("weather_text", "").lower()
+    precipitation_probability = weather_data.get("PrecipitationProbability", 0)
+
+    if temperature < -10: #понизила, иначе по России всегда слишком холодно
+        return "Weather is not fine: Too cold. (Stay home and drink cocoa)."
+    if temperature > 35:
+        return "Weather is not fine: Too hot. (Both weather and you)"
+    if wind_speed and wind_speed > 50:
+        return "Whether is not fine: Strong wind (You will be carried away, darling)"
+    if humidity and humidity > 80:
+        return "Whether is not fine: High humidity (It is harder to breath, and not because of new Billie Eilish song coming out)"
+    if precipitation_probability > 65:
+        return "Weather is not fine: High chance of precipitation (Take your fancy pink umbrella)."
+    if any(condition in weather_text for condition in ["rain", "snow", "storm"]):
+        return "Whether is not fine: Precipitation expected (rain, snow, or storm). Too dangerous for you, darling."
+
+    return "Weather is fine!!! Go and have fun outside!"
 
 @app.route('/')
 def home():
